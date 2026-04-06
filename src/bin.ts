46efec919c7bd220b84a03eb0614976ba72f41eb
Vladimir Bulyga <zero@13w.me>
2026-04-06 20:14:38 +0100

    feat: bin.ts serve command uses http-server.ts

--- src/bin.ts
+++ src/bin.ts
@@ -3,7 +3,8 @@ const args = process.argv.slice(2).filter((a) => a !== "--");
 const cmd = args[0];
 
 if (cmd === "serve" || cmd === "server") {
-  await import("./server.js");
+  const { startHttpServer } = await import("./http-server.js");
+  await startHttpServer();
 } else if (cmd === "init") {
   const { init } = await import("./init.js");
   init();
